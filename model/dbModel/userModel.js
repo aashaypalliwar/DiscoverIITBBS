99074a6dc3469e734c4d774299325b5e24607c89
kartikeya2002 <raj.karthikeya2002@gmail.com>
2020-10-19 19:58:04 +0530

    added user schema

---
+++ model/dbModel/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+const userSchema = new mongoose.Schema({
+
+    name : {
+        type:String,
+        required: [true, 'name of the user should be specified'],
+    },
+    email:{
+        type:String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    bio :{
+        type:String,
+        required: [true, 'Please provide your bio'],
+    },
+    Tags : [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tag'
+        }
+    ],
+    role:{
+        type:String,
+        enum: ['user', 'Admin','superAdmin'],
+        default: 'user',
+    }
+
+    
+
+},{
+    toJSON : {virtuals : true},
+    toObject : {virtuals:true}
+
+});
+
+const User = mongoose.model('User',userSchema);
+module.exports = User;
