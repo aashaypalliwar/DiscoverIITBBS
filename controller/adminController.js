e2cbc738f5961540b75ed8251c151ae64eab89e4
kartikeya-123 <56500864+kartikeya-123@users.noreply.github.com>
2020-10-24 16:59:07 +0530

    added functionality of verify and publish

--- controller/adminController.js
+++ controller/adminController.js
@@ -8,48 +8,59 @@ exports.unpublish = catchAsync(async(req,res,next)=>{
    
     const emails_to_depublish= req.body.emails;
     // console.log(emails_to_depublish);
+
     if(!emails_to_depublish){
         return next(new AppError('There are no emails in the request',401));
     }
-    roles = ['user'];
+
+    // Restriction for depublishing
+    roles = ['user','admin'];
     if(req.user.role==="superAdmin"){
-        roles.push('Admin');
+        roles.push('superAdmin');
     }
+
+    // Updating 
     const updatedResponse = await User.updateMany({email : {$in : emails_to_depublish},role:{$in : roles},publishStatus:{$eq : true}},{$set : {publishStatus : false}})
     .catch(err=>console.log(err));
     
-    
-    const unPublishFailedUsers = await User.find({email : {$in :emails_to_depublish}});
+    //Getting the updated documents
+    const updatedDocument = await User.find({email : {$in :emails_to_depublish}});
     // console.log(updatedUsers);
+
+    //Success and Failure email arrays
     unpublish_failed_user_emails = [];
     unpublish_success_user_emails = [];
 
-    for ( let user of unPublishFailedUsers){
+    for ( let user of updatedDocument){
         if(user.publishStatus)unpublish_failed_user_emails.push(user.email);
         else unpublish_success_user_emails.push(user.email);
     }
     
-   
-        await sendEmail({
-            email : unpublish_success_user_emails,
-            subject : "From Discovery Portal",
-            message : "You are unpublished by the superAdmin",
+    try{          
+        // Sending email to users  
+        // await sendEmail({
+        //     email : unpublish_success_user_emails,
+        //     subject : "From Discovery Portal",
+        //     message : "You are unpublished by the superAdmin",
            
-        });
+        // });
     
-    if(unpublish_failed_user_emails.length===0)
-    {
-          res.status(200).json({
+        if(unpublish_failed_user_emails.length===0)
+        {
+            res.status(200).json({
                   status:'success',
-      
             });
-    }
-    else{
-        res.status(200).json({
-            unpublish_failed : unpublish_failed_user_emails
-        })
-    }
+        }
+        else
+        {
+            res.status(200).json({
+                unpublish_failed_users : unpublish_failed_user_emails
+            });
+        }
 
+    }catch(err){
+        return next (new AppError('Error sending mail to the user',401));
+    }
 
 });
 
@@ -57,34 +68,51 @@ exports.publish = catchAsync(async(req,res,next)=>{
    
     const emails_to_publish = req.body.emails;
     // console.log(emails_to_publish);
+
     if(!emails_to_publish){
         return next(new AppError('There are no emails in the request',401));
     }
-    
+    //Updating
     const updatedResponse = await User.updateMany({email : {$in : emails_to_publish},role:{$in : ['user','admin']},publishStatus:{$eq : false}},{$set : {publishStatus : true}})
     .catch(err=>console.log(err));
     
-    
-    const PublishFailedUsers = await User.find({email : {$in :emails_to_publish},publishStatus:{$eq : false}});
+    // Getting Updated documents
+    const updatedDocument = await User.find({email : {$in :emails_to_publish}});
     // console.log(updatedUsers);
+
+    //Success and Failure arrays
     publish_failed_user_emails = [];
-    for ( let user of PublishFailedUsers){
-        publish_failed_user_emails.push(user.email);
+    publish_success_user_emails = [];
+
+    for ( let user of updatedDocument){
+        if(user.publishStatus)publish_success_user_emails.push(user.email);
+        else publish_failed_user_emails.push(user.email);
     }
     
-    if(publish_failed_user_emails.length===0)
-    {
-          res.status(200).json({
-                  status:'success',
-      
-            });
-    }
-    else{
-        res.status(200).json({
-            publish_failed : publish_failed_user_emails
-        })
-    }
+    try{
+            // await sendEmail({
+            //    email : pubish_success_user_emails,
+            //    subject : "From Discovery Portal",
+            //    message : "You are published by the superAdmin",
+           
+            //  });
 
+        if(publish_failed_user_emails.length===0)
+            {
+                 res.status(200).json({
+                 status:'success',
+                  });
+            }
+         else
+            {
+                res.status(200).json({
+                publish_failed : publish_failed_user_emails
+                })
+             }
+
+        }catch(err){
+            return next (new AppError('Error sending mail to the user',401)); 
+        }
 
 });
 
@@ -96,32 +124,49 @@ exports.unverify = catchAsync(async(req,res,next)=>{
     if(!emails_to_unverify){
         return next(new AppError('There are no emails in the request',401));
     }
-    roles = ['user'];
+
+    //Restriction
+    roles = ['user','admin'];
     if(req.user.role==="superAdmin"){
-        roles.push('Admin');
+        roles.push('superAdmin');
     }
     const updatedResponse = await User.updateMany({email : {$in : emails_to_unverify},role:{$in : roles},verifyStatus:{$eq : true}},{$set : {verifyStatus : false}})
     .catch(err=>console.log(err));
     
     
-    const unverifyFailedUsers = await User.find({email : {$in :emails_to_unverify},verifyStatus:{$eq : true}});
+    const updatedDocument = await User.find({email : {$in :emails_to_unverify}});
     // console.log(updatedUsers);
     unverify_failed_user_emails = [];
-    for ( let user of unverifyFailedUsers ){
-        unverify_failed_user_emails.push(user.email);
+    unverify_success_user_emails = [];
+
+    for ( let user of updatedDocument ){
+        if(user.verifyStatus)unverify_failed_user_emails.push(user.email);
+        else unverify_success_user_emails.push(user.email);
     }
     
-    if(unverify_failed_user_emails.length===0)
-    {
-          res.status(200).json({
-                  status:'success',
+    try{
+        // await sendEmail({
+        //     email : unverify_success_user_emails,
+        //     subject : "From Discovery Portal",
+        //     message : "You are published by the superAdmin",
+        
+        //   });
+
+        if(unverify_failed_user_emails.length===0)
+             {
+                   res.status(200).json({
+                      status:'success',
       
-            });
-    }
-    else{
-        res.status(200).json({
-            unpublish_failed : unverify_failed_user_emails
-        })
+                       });
+             }
+        else{
+                  res.status(200).json({
+                   unverify_failed : unverify_failed_user_emails
+                  })
+            }
+
+    }catch(err){
+        return next(new AppError('Error in sending the mail',401));
     }
 
 
@@ -134,32 +179,46 @@ exports.verify = catchAsync(async(req,res,next)=>{
     if(!emails_to_verify){
         return next(new AppError('There are no emails in the request',401));
     }
-    roles = ['user'];
+    roles = ['user','admin'];
     if(req.user.role==="superAdmin"){
-        roles.push('Admin');
+        roles.push('superAdmin');
     }
     const updatedResponse = await User.updateMany({email : {$in : emails_to_verify},role:{$in : roles},verifyStatus:{$eq : false}},{$set : {verifyStatus : true}})
     .catch(err=>console.log(err));
     
     
-    const verifyFailedUsers = await User.find({email : {$in :emails_to_verify},verifyStatus:{$eq : false}});
+    const updatedDocument= await User.find({email : {$in :emails_to_verify}});
     // console.log(updatedUsers);
     verify_failed_user_emails = [];
-    for ( let user of verifyFailedUsers){
-        verify_failed_user_emails.push(user.email);
+    verify_success_user_emails = [];
+
+    for ( let user of updatedDocument){
+        if(user.verifyStatus)verify_success_user_emails.push(user.email);
+        else verify_failed_user_emails.push(user.email);
     }
     
-    if(verify_failed_user_emails.length===0)
-    {
-          res.status(200).json({
-                  status:'success',
-      
-            });
-    }
-    else{
-        res.status(200).json({
-            verify_failed : verify_failed_user_emails
-        })
+    try{
+        // await sendEmail({
+        //     email : verify_success_user_emails,
+        //     subject : "From Discovery Portal",
+        //     message : "You are published by the superAdmin",
+        
+        //   });
+
+        if(verify_failed_user_emails.length===0)
+             {
+                   res.status(200).json({
+                      status:'success',
+                      });
+             }
+        else{
+                  res.status(200).json({
+                   verify_failed : verify_failed_user_emails
+                  })
+            }
+
+    }catch(err){
+        return next(new AppError('Error in sending the mail',401));
     }
 
 
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,8 +5,28 @@ const adminController = require('./../controller/adminController');
 const router = express.Router();
 
 
-router.patch('/unpublish',authLogic.verifyJwtToken,authLogic.restrictTo('admin','superAdmin'),authLogic.loggedInUser,adminController.unpublish);
-router.patch('/publish',authLogic.verifyJwtToken,authLogic.restrictTo('superAdmin'),authLogic.loggedInUser,adminController.publish);
-router.patch('/verify',authLogic.verifyJwtToken,authLogic.restrictTo('admin','superAdmin'),authLogic.loggedInUser,adminController.verify);
-router.patch('/unverify',authLogic.verifyJwtToken,authLogic.restrictTo('admin','superAdmin'),authLogic.loggedInUser,adminController.unverify);
+router.patch('/unpublish',
+      authLogic.verifyJwtToken,
+      authLogic.restrictTo('admin','superAdmin'),
+      authLogic.loggedInUser,
+      adminController.unpublish);
+
+router.patch('/publish',
+      authLogic.verifyJwtToken,
+      authLogic.restrictTo('superAdmin'),
+      authLogic.loggedInUser,
+      adminController.publish);
+
+router.patch('/verify',
+      authLogic.verifyJwtToken,
+      authLogic.restrictTo('admin','superAdmin'),
+      authLogic.loggedInUser,
+      adminController.verify);
+
+router.patch('/unverify',
+      authLogic.verifyJwtToken,
+      authLogic.restrictTo('admin','superAdmin'),
+      authLogic.loggedInUser,
+      adminController.unverify);
+
 module.exports = router;
